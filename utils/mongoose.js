3ecce11cf3b1d6b99c5d144232719de95e6acce2
wellington Rochina <wellirochi89@gmail.com>
2022-06-26 13:23:55 -0500

    feat: add security environment variables

--- utils/mongoose.js
+++ utils/mongoose.js
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose'
-import { MONGODB_URI } from '../config.js'
+import dotenv from 'dotenv'
+const dotEnv=dotenv.config()
 
 export async function connectToDB(){
   try{
-    await mongoose.connect(MONGODB_URI)
+    await mongoose.connect(process.env.MONGODB_URI)
     console.log('Mongod connected')
   }catch (error){
     console.error(error)
